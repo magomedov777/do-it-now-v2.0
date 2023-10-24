705064b9ca4c982dee5b7cc6115c665b64b06950
Arkadiy <alibullterrier@gmail.com>
2023-10-24 14:38:47 +0300

    create: JS doc for create-app-async-thunk func

--- src/utils/create-app-async-thunk.ts
+++ src/utils/create-app-async-thunk.ts
@@ -1,8 +1,16 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import {AppDispatch, AppRootStateType} from "app/store";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { AppDispatch, AppRootStateType } from "app/store";
+
+/**
+ * Create an async thunk for the app.
+ *
+ * @template {Object} TState - The type of the state.
+ * @template {Function} TDispatch - The type of the dispatch function.
+ * @returns {AsyncThunk<null, void, { state: TState, dispatch: TDispatch, rejectValue: null }>} The async thunk for the app.
+ */
 
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{
-    state: AppRootStateType
-    dispatch: AppDispatch
-    rejectValue: null
-}>()
+  state: AppRootStateType;
+  dispatch: AppDispatch;
+  rejectValue: null;
+}>();
