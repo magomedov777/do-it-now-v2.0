cb9a64e1179f109430e4e07ec20f0556111a4593
Arkadiy <alibullterrier@gmail.com>
2023-10-24 14:53:57 +0300

    create: JS Doc for useAppDispatch

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -1,4 +1,10 @@
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../app/store";
 
+/**
+ * Custom hook to get the typed `dispatch` function from Redux.
+ *
+ * @returns {AppDispatch} The typed `dispatch` function.
+ */
+
 export const useAppDispatch = () => useDispatch<AppDispatch>();
